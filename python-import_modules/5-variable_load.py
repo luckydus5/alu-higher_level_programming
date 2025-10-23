4da7ca2ce25bc2ad308c871ac4b7b1f4e075d431
OLivier Dusabamahoro <o.dusabamah@alustudent.com>
2025-10-23 07:47:44 +0200

    program that imports the variable a from the file variable_load_5.py and prints its value

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+# A script that imports variable 'a' from variable_load_5.py and prints its value.
+
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
