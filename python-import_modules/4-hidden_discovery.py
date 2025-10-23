77b7dfbbcd9f8198244830f8f5075971fae504de
OLivier Dusabamahoro <o.dusabamah@alustudent.com>
2025-10-23 07:42:58 +0200

    program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# 4-hidden_discovery.py
+# A script that prints all names defined in the compiled module hidden_4.pyc,
+# excluding names that start with "__".
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
