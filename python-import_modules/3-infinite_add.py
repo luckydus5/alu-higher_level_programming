af0533aeca5827929c7ef00f0e4f84b11e114bd5
OLivier Dusabamahoro <o.dusabamah@alustudent.com>
2025-10-23 07:39:56 +0200

    Imports sys to read command-line arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# 3-infinite_add.py
+# A script that prints the result of the addition of all arguments.
+
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]  # exclude script name
+    total = 0
+
+    for arg in args:
+        total += int(arg)  # cast argument to int and add to total
+
+    print(total)
